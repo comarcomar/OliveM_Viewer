215f555c5295986d001e3c920968b91d59902030
comarcomar <49150382+comarcomar@users.noreply.github.com>
2025-12-28 12:32:19 +0100

    Add reset and cleanup methods to GeoTiffProcessor

--- geotiffprocessor.h
+++ geotiffprocessor.h
@@ -4,57 +4,51 @@
 #include <QObject>
 #include <QString>
 #include <QImage>
-#include <QQuickImageProvider>
-#include <QThread>
-
-// Forward declaration for GDAL
-class GDALDataset;
+#include <gdal_priv.h>
 
 class GeoTiffProcessor : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(bool hasValidImages READ hasValidImages NOTIFY imagesChanged)
 
 public:
     explicit GeoTiffProcessor(QObject *parent = nullptr);
     ~GeoTiffProcessor();
 
-    bool hasValidImages() const;
+    // Methods to load and process GeoTIFF images
+    bool loadImage1(const QString &filePath);
+    bool loadImage2(const QString &filePath);
+    
+    // Getters
+    QString getImage1Path() const { return m_image1Path; }
+    QString getImage2Path() const { return m_image2Path; }
+    bool hasImage1() const { return m_hasImage1; }
+    bool hasImage2() const { return m_hasImage2; }
+    QImage getImage1() const { return m_image1; }
+    QImage getImage2() const { return m_image2; }
 
 public slots:
-    void setImage1(const QString &path);
-    void setImage2(const QString &path);
-    void runAnalysis();
+    // Reset method to clear all loaded images and state
+    void reset();
 
 signals:
     void imagesChanged();
-    void analysisCompleted(const QString &resultPath, double param1, double param2);
     void errorOccurred(const QString &errorMessage);
 
 private:
+    // Private cleanup method to close open GDAL datasets
+    void cleanup();
+
+    // Member variables
     QString m_image1Path;
     QString m_image2Path;
     bool m_hasImage1;
     bool m_hasImage2;
-
-    // Load GeoTIFF and validate
-    bool loadGeoTiff(const QString &path);
+    QImage m_image1;
+    QImage m_image2;
     
-    // Call external DLL function
-    bool callRunAnalysis(const QString &image1, const QString &image2, 
-                         QString &outputPath, double &param1, double &param2);
-};
-
-// Image provider for displaying GeoTIFF with color maps
-class GeoTiffImageProvider : public QQuickImageProvider
-{
-public:
-    GeoTiffImageProvider();
-    
-    QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize) override;
-
-private:
-    QVector<QColor> getColorMapColors(int index);
+    // GDAL dataset pointers
+    GDALDataset *m_dataset1;
+    GDALDataset *m_dataset2;
 };
 
 #endif // GEOTIFFPROCESSOR_H
